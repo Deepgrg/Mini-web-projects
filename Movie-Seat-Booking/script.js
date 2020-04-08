42dce8bb972457cae62af1b3cf250f51c0413ed9
Dip Gurung <deepgrg03@gmail.com>
2020-04-08 10:47:57 +0545

    Lcal storage applied to save the selected items by the user

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -6,9 +6,20 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+//save selected movie price and index
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
+//
 function updateSelectCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map((seat)=> [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
@@ -19,6 +30,7 @@ function updateSelectCount(){
 //Movie select event
 movieSelect.addEventListener('change', (e)=>{
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectCount();
 })
 
